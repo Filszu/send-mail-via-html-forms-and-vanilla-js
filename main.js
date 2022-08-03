40bb37c8ec2f41a0b525d51ca3a4eaa228f56bef
Filszu <filipszumowski05@gmail.com>
2022-08-03 20:52:28 +0200

    app is workig perfecly

--- main.js
+++ main.js
@@ -43,54 +43,65 @@ function searchParams(){
             aaa: 'bbb'
         }
 */
-
+const templ1 = "<h1>Witaj</h1><p>{{name}}</p><p>{{email}}<p>{{wrongKey}}<h2>{{luckyNumber}}</h2><h3>{{wrongKey}}</h3><hr>";
+const templ2 = "<h1>name: {{name}}</h1>";
+const templ3 = "Abcd hej"
 const parserConfig = {
-    template: "<h1>Witaj</h1><p>{{name}}</p><p>{{email}}<p>{{wrongKey}}<h2>{{luckyNumber}}</h2><h3>{{wrongKey}}</h3><hr>",
+    template: templ1,
     fsValues: null
 }
 
 //wyszkuwianie znaczka kodu {{}} i zamiana domyslenego tekstu z szablonu na ten gotowy
 function parser(){
 
-    template = parserConfig.template;
+    template = parserConfig.template;//templatka do podmiany
     fsValues = parserConfig.fsValues;//tu chodzi o object
     // template = template.split("");
-    let i=0;
-    do{
 
+
+    //wyszukiwanie czy insteje to w stringu
+    function fsTagsPositions(el){
+
+        return template.indexOf(el)
+    }
+
+    fsTagStartPosition = fsTagsPositions("{{")
+    fsTagEndPosition = fsTagsPositions("}}")
+
+    while((fsTagStartPosition>=0)&&((fsTagEndPosition>=0))){
+        //wyszukiwanie tagow
+
+        //szukane slowo
+        let fsKeyWord = template.slice(fsTagStartPosition+2, fsTagEndPosition)
+
+        //ciąg do podmnienienia z {{ klucz}}
+        let fsKeyToParse = template.slice(fsTagStartPosition, fsTagEndPosition+2)
+
+        console.log(fsKeyWord)
+        console.log(fsKeyToParse)
+        
+        //spradzanie czy istnieje i podmienianie
+        if(fsKeyWord in fsValues){
+            console.log("key has been founded: "+fsKeyWord)
+            template =template.replace(fsKeyToParse,fsValues[fsKeyWord])
+            
+
+            // console.log(fsValues[fsKeyWord])
+        }
+        else {
+            console.log("there is no such key like: "+fsKeyWord)
+
+            template =template.replace(fsKeyToParse,'NULL')
+            //  undefined
+        }
+        console.log(`%c---\n ${template}`, "color: lime");
+
+        fsTagStartPosition = fsTagsPositions("{{")
+        fsTagEndPosition = fsTagsPositions("}}")
+        console.log(fsTagStartPosition, fsTagEndPosition)
+
+    }
     
-    //wyszukiwanie tagow
-    fsTagStartPosition = template.indexOf("{")
-    fsTagEndPosition = template.indexOf("}")
-    console.log(fsTagStartPosition, fsTagEndPosition)
-
-    //szukane slowo
-    let fsKeyWord = template.slice(fsTagStartPosition+2, fsTagEndPosition)
-
-    //ciąg do podmnienienia z {{ klucz}}
-    let fsKeyToParse = template.slice(fsTagStartPosition, fsTagEndPosition+2)
-
-    console.log(fsKeyWord)
-    console.log(fsKeyToParse)
-
-    //spradzanie czy istnieje i podmienianie
-    if(fsKeyWord in fsValues){
-        console.log("key has been founded: "+fsKeyWord)
-        template =template.replace(fsKeyToParse,fsValues[fsKeyWord])
-
-        // console.log(fsValues[fsKeyWord])
-    }
-    else {
-        console.log("there is no such key like: "+fsKeyWord)
-
-         template =template.replace(fsKeyToParse,'NULL')
-        //  undefined
-    }
-
-    
-    i++
-    }
-    while((fsTagStartPosition>=0) && (fsTagEndPosition>=0))
     // while(i<10)
 
 
@@ -121,7 +132,7 @@ parseredBox.innerHTML=view;
 }
 
 
-console.log(dataToSend)
+
 function sendEmail(){
   
    
@@ -136,3 +147,4 @@ const dataToSend = {
     content: {}
 
 }
+console.log(dataToSend)
